7d4a809dc1ef0dcca24c9b59fa31771d952e276c
SKPant <sanjayakumarpant@gmail.com>
2026-02-03 19:02:52 +0900

    Create credentials_store.py

---
+++ credentials_store.py
@@ -0,0 +1,16 @@
+# credentials_store.py
+import streamlit as st
+
+def get_service_account_info():
+    return {
+        "type": "service_account",
+        "project_id": "chem-xai-project",
+        "private_key_id": "09d9edc434243d5f48924e6dfb83331401965d8e",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDhcHL6DXQMvtZE\nEdp+wDqdQhh//mAe+ITZFMBVnhmYiT5Sq0CnWACyRyWKbOcx2y3mAUj7vwDmYBD0\n3KdNyLh2uEzgYFzpo7fU/LLP2O5tb9vBtRH4rZDCCGMEeYjI+LosT0oa0SCobF3P\njmEKQ7ZF9ftysHh0buTYzR0Y2ZsIJJpNLba1jOjPMfLfm3KffTf8mKsI0rncYlqw\npuWJ8GqmK5sRBnNUtE+5DvtakKDFHqi5CiJsHfhDshQZrGJxdfMoQePgcN715bWZ\ksf8ETT8Ix6Jn3WVfX56Tgw0KANPRGf+XpSdiJP3FX+jbnSdEYbxjuz8zFXMJSdL\n6qfnPHa7AgMBAAECggEAaS7PnmZ/gmDZ1MmlsaTb1Dqd9r4HN/wI88XwVSIeXCKQ\rv6S/GEddhCm7daQ6hyS5jEsTbUY5OPmlXCMKMkhc9bam4eqYiJOZ7P9c+eJmwrp\nNBBZEbddhoJmPJS94tLV/k2CTE/Nlnd1L52FHkZoeWzjBGhc0ypYYOUWkd0ZreqR\nb+MeAYcfj+KttTDih8pLb4bCximDX7HDEfzfiTGtLAgQqsBmmHylznjUBfMIJA0s\nIR8HVqmDqaUyjm3Q4BVjx8+FX6qDxSSS+KOEzHYCR2nXbR8g9AydRs2EFTZzxPp9\n1YGKQaxrgMCaI2OoxTGSIHkB3+FZ2ySY/2IRunZTFQKBgQD/goUZxhkDAdf1OJIL\ni9auxZmpsBnauBAUzqr4ZaY0eVqihu/VzY7oSuOFCQZSPcoSuOFCQZSPcoSpX4Y0\nKVxK/QVeQBSlhpD+BZ0BUKP6HwlTFLB+Mok8RmNdHeUTPNxd7Fom56Em2Ulgn7Htp\nJqJdR7XdlPGNTXIPmS82J2mDInA6dY2BsaZQKBgQDh3ylgvP4Zue0TlaBDWetrmYs\nT12HeJeX9Q8AlCKbOA+H9ZIJhBe+3DHYURQxsDFczdlmP4w3YyGieJUv1noyB/US\np2iImOCTyXfSAUuE0q3Z2HC61SqALsGArpbOjG/frgQlXn5l4uKHgF6WhWkV/QWR\nwSbHj1pO7SkdlWCqnwKBgBOxQIbjAKx9qOJKcN8Y8PvZWOV+IA/+Xabs4rpwQyYK\nMFUUZ0moJRp8IxruviD6iCd1v8kz03xsccxx3fd/gBi69ygL+7xRo7fQMRSoExRT\nzK9dSZYRdDXLjs3MKKR5wYrpitHjnVc3ATBc5FG3TTgjX3jW70DKrBrbqjsrtLM\nNAoGADVPYoi+koqn41i3+/dphbMDrlukfijccQfxsawL+rKtH+2ah1s293kQq93\nk3iWyU/KFgGYBhQchZe/NhbwXC7qyeENz06xJyNeYuXsLazOSNg4wk4rFPR676l\nfg3R8q7kyw2bGoN9T8U9L/bVPBcSgLbAEWwXxgTURLnWSqGQKsCgYAL32sLF2sX\nQoYjl1KUZGH9/qareoRn5KqmUjhnIGm7o+f4A3qR8C8NbRZA4O08PLRrhpBqlad\nObo7gSnmSysY9nu4vYn4PWHUXiJYtC35H8TduXufDMYjENJLXnP34x/shKvDhSG\n8gP/IIK2P+d8+UGS8BH4qZqElxTO1JRtdp+/A==\n-----END PRIVATE KEY-----\n",
+        "client_email": "streamlit-research-access@chem-xai-project.iam.gserviceaccount.com",
+        "client_id": "116425675333935406982",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/streamlit-research-access%40chem-xai-project.iam.gserviceaccount.com"
+    }
